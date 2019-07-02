36ec13f5b0c6618bca00a1f4a9ffba6359f4c278
owlscode <XKQN0644@ad-subs.w2k.francetelecom.fr>
2019-07-02 11:39:54 +0200

    Add change to contact index template

--- template/template.php
+++ template/template.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <link href="<?php site_url(); ?>/template/style.css" rel="stylesheet" type="text/css" /> 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <div class="wrap">
