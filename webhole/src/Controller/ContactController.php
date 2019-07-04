4333d561f68a1ee25ac89df49e1cec11b44f0cd4
owlscode <hippolyte@WX-OR7507715.ad-subs.w2k.francetelecom.fr>
2019-07-04 14:53:36 +0200

    Translate project to Symfony

---
+++ webhole/src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig');
+    }
+
+
+    /**
+     * @Route("/contact)
+     */
+}
