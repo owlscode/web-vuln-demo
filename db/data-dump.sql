1c1ee454efdce226cc28106084de668f911899e5
hippwn <>
2019-07-17 10:08:22 +0200

    Add support page, fix things, refactor again

--- db/data-dump.sql
+++ db/data-dump.sql
@@ -13,24 +13,41 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `WebHole` /*!40100 DEFAULT CHARACTER SE
 USE `WebHole`;
 
 --
--- Table structure for table `Utilisateur`
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` char(10) DEFAULT NULL,
-  `password` char(100) DEFAULT NULL,
-  `creditCardNumber` char(50) DEFAULT NULL,
-  `role` char(10) DEFAULT NULL,
-  `firstname` char(20) DEFAULT NULL
+  `id` varchar(10) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `creditCardNumber` varchar(50) DEFAULT NULL,
+  `role` varchar(10) DEFAULT NULL,
+  `firstname` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Utilisateur`
+-- Dumping data for table `user`
 --
 
 -- LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES ('aaaa','8a9a60a0ded26ca6e68132b039383f94e580a493c1e8e540ac18faa8ba959caf','XKQNBCOIUY','admin','George'),('bbbb','b37dfcdb9677b8c8ae9f3e0d5268afc40ac89f25757e1fab9a9d74ef3352a00a','APEOLDNFJF','user','Alphonse');
 UNLOCK TABLES;
+
+
+--
+-- Table structure for table `support`
+--
+
+DROP TABLE IF EXISTS `support`;
+CREATE TABLE `support` (
+  `id` varchar(32) NOT NULL,
+  `user` varchar(10) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `subject` varchar(256) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `filename` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- Dump completed on 2019-07-08 14:10:02
--- webhole/src/Controller/ContactController.php
+++ webhole/src/Controller/ContactController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use \PDO;
 
 
 class ContactController extends AbstractController
@@ -38,23 +39,18 @@ class ContactController extends AbstractController
             ->getForm();
 
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $task = $form->getData();
-
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
-
+            
+            $data = $form->getData();
+            $requestId = md5($data['email'] . time());
+            
+            
             $file = $form['file']->getData();
             if ($file) {
                 $originalFilename = $file->getClientOriginalName();
-                $newFilePath = 'uploads/' . md5($originalFilename . time()) . '/';
-
+                $newFilePath = 'uploads/' . $requestId . '/';
+                
                 // Move the file to the directory where brochures are stored
                 try {
                     $file->move(
@@ -64,16 +60,62 @@ class ContactController extends AbstractController
                 } catch (FileException $e) {
                     // ... handle exception if something happens during file upload
                 }
-
+                
             }
+            
+            $u = (isset($_COOKIE['rank'])) ? unserialize(base64_decode($_COOKIE['rank']))['id'] : "";
+            $f = $originalFilename ?? "";
+            
+            try {
+                $db = new PDO('mysql:host=db;dbname=WebHole', 'root', 'root');
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $stmt = $db->prepare("INSERT INTO support VALUES (?, ?, ?, ?, ?, ?)");
+                $stmt->bindParam(1, $requestId);
+                $stmt->bindParam(2, $u); // if id is set in $_COOKIE
+                $stmt->bindParam(3, $data['email']);
+                $stmt->bindParam(4, $data['subject']);
+                $stmt->bindParam(5, $data['message']);
+                $stmt->bindParam(6, $f);
 
-            //return $this->redirectToRoute('task_success');
+                $stmt->execute();
+            } catch (Exception $e) {
+                print "tott";
+            }
+            
+            return $this->render('contact/preview.html.twig', [
+                'link' => '/contact/' . $requestId,
+            ]);
         }
 
-        return $this->render('contact.html.twig', [
-            'test' => 'this is a test',
+        return $this->render('contact/send.html.twig', [
             'form' => $form->createView(),
         ]);      
     }
    
+    /**
+     * @Route("/contact/{id}", requirements={"id"="[a-f0-9]{32}"})
+     */
+    public function view($id) {
+        try {
+
+            $db = new PDO('mysql:host=db;dbname=WebHole', 'root', 'root');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+            $stmt = $db->prepare("SELECT * FROM support WHERE id = ?");
+            $stmt->execute(array($id));
+            
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            var_dump($rows);
+        } catch (PDOException $e) {
+            print "ttojzgfns";
+        }
+
+        if (sizeof($rows) != 1) {
+            throw $this->createNotFoundException('The page does not exist.');
+        }
+
+        return $this->render('contact/view.html.twig', [
+            'request' => $rows[0]
+        ]);
+    }
 }
